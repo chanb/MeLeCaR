4afdecc65361024b784e5cb28e2d89b251dc0500
chanb <bryanchan13@gmail.com>
2019-04-05 16:11:58 -0400

    Updated generate data to return more info

--- rl_envs/__init__.py
+++ rl_envs/__init__.py
@@ -4,5 +4,5 @@ for cache_size in [10, 30]:
   register(
     "Cache-Bandit-C{}-{}-v0".format(cache_size, "casa"),
     entry_point="rl_envs.cache_bandit:CacheBandit",
-    kwargs={"cache_size": cache_size, "workload": "casa-110108-112108.6.blkparse", "max_requests": 100000}
+    kwargs={"cache_size": cache_size, "workload": "casa-110108-112108.6.blkparse", "max_requests": 10000}
   )
