ef3e2f676f4ccb044ad5769f226359c1e583fd88
赵祺 <stevezhao13@hotmail.com>
2019-04-30 12:49:45 -0400

    final version

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 from tqdm import tqdm_notebook as tqdm
+import csv
 
 from config import *
 from models.baseline_classifier import Baseline
@@ -37,7 +38,7 @@ def Y_getBlockSeq(Y_pred_prob, eviction):
 	idx = idx[:eviction]
 	return idx
 
-def predict(train_data, trained_model_file_name, predict_pkl_data, predict_raw_file, data_percentage, batch_size=10, cache_size=100):
+def predict(train_data, trained_model_file_name, predict_pkl_data, predict_raw_file, data_percentage, save_result_file, batch_size=10, cache_size=100):
 	blocktrace = get_block_trace(predict_raw_file, data_percentage)
 	# LFUDict = defaultdict(int)
 	LRUQ = []
@@ -46,6 +47,10 @@ def predict(train_data, trained_model_file_name, predict_pkl_data, predict_raw_f
 	loop_num = 0
 	C = []
 	evictCacheIndex = np.array([])
+	result_file_name = save_result_file + ".txt"
+	f = open(result_file_name,"w+")
+	# csv_file = open(result_file_name, 'w+')  
+	# csv_writer = csv.writer(csv_file, delimiter=',')
 	for seq_number, block in enumerate(tqdm(blocktrace, desc="OPT")):
 		# LFUDict[block] +=1
 		loop_num += 1
@@ -69,19 +74,6 @@ def predict(train_data, trained_model_file_name, predict_pkl_data, predict_raw_f
                     #return Y_pred_prob, evictCacheIndex
                 # evict from cache
 				evictPos = evictCacheIndex[0]
-				# print("test: print out the biggest probability position:")
-				# print(Y_pred_prob[evictPos]) # test: print out the biggest probability position
-				# print("evictPos:")
-				# print(evictPos)
-				# print("evictCacheIndex")
-				# print(evictCacheIndex)
-				# print("Y_pred_prob")
-				# print(Y_pred_prob.shape)
-				# print(Y_pred_prob[-1])
-				# print("cache content")
-				# print(C)
-				# print("pred_inputs")
-				# print(pred_inputs[0][0])
 				evictBlock = C[evictPos]
 				LRUQ.remove(evictBlock)
 			if evictPos is -1:
@@ -90,10 +82,19 @@ def predict(train_data, trained_model_file_name, predict_pkl_data, predict_raw_f
 				C[evictPos] = block
 				evictCacheIndex = np.delete(evictCacheIndex, 0)
 			LRUQ.append(block)
-		if(loop_num%2000==0):
-			print(hit / (hit + miss))
+		if(loop_num%100==0):
+		    # csv_writer.writerow(loop_num)
+		    # csv_writer.writerow(hit/(hit + miss))
+			f.write("%d\n" % loop_num)
+			f.write("%f\n" % (hit/(hit + miss)))
+			# f.write("loop number= %d\r\n" % loop_num)
+			# f.write("hit rate= %f\r\n" % (hit/(hit + miss)))
+	f.close()
 	hitrate = hit / (hit + miss)
-	print(hitrate)
+	f = open(result_file_name,"a+")
+	f.write("%d\n" % loop_num)
+	f.write("%f\n" % hitrate)
+	f.close()
 	return hitrate
 
 
@@ -104,11 +105,16 @@ if __name__ == '__main__':
   parser.add_argument("--predict_pkl_data", type=str, help="pkl data to predict", required=True)
   parser.add_argument("--predict_raw_file", type=str, help="raw data to predict", required=True)
   parser.add_argument("--data_percentage", type=float, help="predict data percentage", required=True)
+  parser.add_argument("--save_result_file", type=str, help="file to save final result", required=True)
   parser.add_argument("--batch_size", type=int, help="batch size", default=10)
   parser.add_argument("--cache_size", type=int, help="learning rate", default=100)
   args = parser.parse_args()
 
-  pred_hitrate = predict(args.train_data, args.trained_model_file_name, args.predict_pkl_data, args.predict_raw_file, args.data_percentage, args.batch_size, args.cache_size)
+  pred_hitrate = predict(args.train_data, args.trained_model_file_name, args.predict_pkl_data, args.predict_raw_file, args.data_percentage, args.save_result_file, args.batch_size, args.cache_size)
   # first test: hit rate = 0.03606070741700811
   # parameter:
   # python predict.py --train_data=webmail_16.pkl --trained_model_file_name=./train_output/webmail_16_train_output-gru.h5 --predict_pkl_data=online_09.pkl --predict_raw_file=online.cs.fiu.edu-110108-113008.9.blkparse --data_percentage=0.1 --batch_size=100 --cache_size=100
+
+  # second: hit rate = 0.03606070741700811
+  # python predict.py --train_data=webmail_12.pkl --trained_model_file_name=./train_output/webmail_12_train_output-gru.h5 --predict_pkl_data=online_09.pkl --predict_raw_file=online.cs.fiu.edu-110108-113008.9.blkparse --data_percentage=0.1 --batch_size=100 --cache_size=100
+  # python predict.py --train_data=online_09.pkl --trained_model_file_name=./train_output/online_09_train_output-gru.h5 --predict_pkl_data=online_12.pkl --predict_raw_file=online.cs.fiu.edu-110108-113008.12.blkparse --data_percentage=0.1 --save_result_file=train_online09_C30_predict_online12 --batch_size=100 --cache_size=100
